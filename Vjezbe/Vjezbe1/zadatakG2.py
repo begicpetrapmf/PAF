68a883fa4695594514e36765482176c77cb362a5
Petra Begic <pbegic@pmfst.hr>
2026-03-18 19:24:35 +0100

    Gibanja:2.-pozivanje funkcije

---
+++ Vjezbe/Vjezbe1/zadatakG2.py
@@ -0,0 +1,6 @@
+# pozivanje funkcije:
+import kinematika
+F=float(input(f'Unesi silu(N):'))
+m=float(input(f'Unesi masu(kg):'))
+t_max=float(input(f'Unesi konačno vrijeme(s):'))
+kinematika.jednoliko_gibanje(F,m,t_max)
